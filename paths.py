210df0e74bbd5efc370cf1118ff1d206ca87790e
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2013-02-05 17:00:21 -0800

    Added paths patch

---
+++ paths.py
@@ -0,0 +1,16 @@
+import os
+
+LOCAL = '/usr/local/bin:/usr/local/sbin:'
+HOME = '/Users/alexgorbatchev'
+NVM = '/Users/alexgorbatchev/.nvm/v0.8.14/bin'
+
+# Sublime's default path is
+# /usr/bin:/bin:/usr/sbin:/sbin
+os.environ['PATH'] += ':'
+os.environ['PATH'] += LOCAL
+os.environ['PATH'] += NVM
+
+print '\n\n--------------'
+print 'pathching path in `User/paths.py`'
+print 'PATH = ' + os.environ['PATH']
+print '--------------\n'
